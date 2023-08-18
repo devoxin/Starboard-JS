3ec61709955d7646636d5a66d825838d3d65fdf3
Devoxin <15076404+Devoxin@users.noreply.github.com>
2023-08-18 23:11:32 +0100

    msg content consistency, performance optimisations

--- starboat.js
+++ starboat.js
@@ -9,20 +9,23 @@ db.prepare('CREATE TABLE IF NOT EXISTS starids (msgid TEXT PRIMARY KEY, starid T
 client.on('messageReactionAdd', async (message, emoji, user) => {
   if (message.channel.type !== 0 || emoji.name !== '⭐') return;
 
-  const channel = client.getChannel(message.channel.id);
+  const channel = message.channel;
   const starboard = channel.guild.channels.find(c => c.name.toLowerCase() === 'starboard');
 
   if (channel.nsfw || !starboard || channel.id === starboard.id) return;
 
-  const msg = await channel.getMessage(message.id);
-  const stars = (await msg.getReaction('⭐', msg.reactions['⭐'].count)).filter(u => u.id !== msg.author.id && !client.users.get(u.id)?.bot).length;
+  const [msg, rawStars] = await Promise.all([
+    channel.getMessage(message.id),
+    client.getMessageReaction(message.channel.id, message.id, '⭐', { limit: 100 })
+  ]);
 
-  if (msg.content.length === 0 && msg.attachments.length === 0 && (!msg.embeds[0] || msg.embeds[0].type !== 'image')) return;
+  const stars = rawStars.filter(u => u.id !== msg.author.id && !client.users.get(u.id)?.bot).length;
 
-  const starId = await getMessageFromDatabase(msg.id);
+  if (!stars || !msg.content.length && !msg.attachments.length && (!msg.embeds.length || msg.embeds[0].type !== 'image')) return;
 
-  if (!starId) {
-    if (!stars) return;
+  const starId = getMessageFromDatabase(message.id);
+
+  if (!starId) { // if newly starred message
     const reference = msg.referencedMessage;
     const referenceContent = reference
       ? (reference.content.length > 512 ? reference.content.substring(0, 509) + '...' : (reference.content.length === 0 ? `[\`No content, jump to message\`](${reference.jumpLink})` : reference.content))
@@ -33,7 +36,7 @@ client.on('messageReactionAdd', async (message, emoji, user) => {
     const videoUrl = resolveVideoAttachment(msg);
 
     const starMsg = await starboard.createMessage({
-      content: `${stars} ⭐ - ${msg.jumpLink}`,
+      content: `${stars} ⭐`,
       embed: {
         color: 0xFDD744,
         author: {
@@ -49,45 +52,45 @@ client.on('messageReactionAdd', async (message, emoji, user) => {
         ] : [],
         image: resolveAttachment(msg),
         timestamp: new Date()
-      }
+      },
+      components: [{ type: 1, components: [{ type: 2, style: 5, label: 'Jump to Message', url: msg.jumpLink }] }]
     });
 
     db.prepare('INSERT INTO starids VALUES (?, ?)').run(msg.id, starMsg.id);
   } else {
-    const starMessage = await starboard.getMessage(starId);
-    if (!starMessage) return;
-    await starMessage.edit(`${stars} ⭐ - <#${msg.channel.id}>`);
+    starboard.getMessage(starId)
+      .catch(() => db.prepare('DELETE FROM starids WHERE msgid = ?').run(message.id))
+      .then(msg => msg.edit(`${stars} ⭐`))
   }
 });
 
 client.on('messageReactionRemove', async (message, emoji, user) => {
   if (message.channel.type !== 0 || emoji.name !== '⭐') return;
 
-  const channel = client.getChannel(message.channel.id);
+  const channel = message.channel;
   const starboard = channel.guild.channels.find(c => c.name.toLowerCase() === 'starboard');
 
   if (!starboard || channel.id === starboard.id) return;
 
-  const msg = await channel.getMessage(message.id);
-  const starId = await getMessageFromDatabase(msg.id);
+  const starId = getMessageFromDatabase(message.id);
   if (!starId) return;
 
-  const starMessage = await starboard.getMessage(starId).catch(() => null);
-  if (!starMessage) return db.prepare('DELETE FROM starids WHERE msgid = ?').run(msg.id);
+  const [msg, starMessage, rawStars] = await Promise.all([
+    channel.getMessage(message.id),
+    starboard.getMessage(starId).catch(() => null),
+    client.getMessageReaction(channel.id, message.id, '⭐', { limit: 100 })
+  ]);
 
-  if (!msg.reactions['⭐']) {
-    db.prepare('DELETE FROM starids WHERE msgid = ?').run(msg.id);
-    return await starMessage.delete();
-  }
+  if (!starMessage) return db.prepare('DELETE FROM starids WHERE msgid = ?').run(message.id);
 
-  const stars = (await msg.getReaction('⭐', msg.reactions['⭐'].count)).filter(u => u.id !== msg.author.id && !client.users.get(u.id)?.bot).length;
+  const stars = rawStars.filter(u => u.id !== msg.author.id && !client.users.get(u.id)?.bot).length;
 
   if (!stars) {
-    db.prepare('DELETE FROM starids WHERE msgid = ?').run(msg.id);
+    db.prepare('DELETE FROM starids WHERE msgid = ?').run(message.id);
     return await starMessage.delete();
   }
 
-  await starMessage.edit(`${stars} ⭐ - ${msg.jumpLink}`);
+  await starMessage.edit(`${stars} ⭐`);
 });
 
 function getMessageFromDatabase(msgid) {
